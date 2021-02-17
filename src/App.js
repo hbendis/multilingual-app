2a6f6e712b727e06a798486ff9ec32e0242d67c9
hbendis <56543636+hbendis@users.noreply.github.com>
2021-02-17 20:24:21 +0100

    merge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
 
 function App() {
+  const { t } = useTranslation();
+
+  function handleClick(lang) {
+    i18next.changeLanguage(lang)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <nav style={{ width: '100%', padding: '1rem 0', backgroundColor:'darkred' }}>
+          <button onClick={()=>handleClick('en')} >
+            English
+          </button>
+          <button onClick={()=>handleClick('ar')} >
+            Arabic
+          </button>
+          <button onClick={()=>handleClick('de')} >
+            German
+         </button>
+         <button onClick={()=>handleClick('it')} >
+            Italian
+         </button>
+        </nav>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <h3>{t('Thanks.1')}</h3>  <h3>{t('Why.1')}</h3> 
+          </p>
+        </header>
     </div>
   );
 }
